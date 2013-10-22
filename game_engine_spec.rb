aa973676d7fbed6cca0f0cb3c2b8347b624d9e38
Rob Gough <robert.gough@onthebeach.co.uk>
2013-10-22 14:16:00 +0100

    test for all of them

--- game_engine_spec.rb
+++ game_engine_spec.rb
@@ -16,9 +16,16 @@ describe 'solver' do
   let (:what_is_question) { "what is 5 + 6" }
   let (:which_is_question) { "which of the following numbers is the largest: 100 101 123" }
   let (:which_is_largest_solver) { Solver.new(which_is_question) }
+  
   it 'should correctly answer what is' do
     expect(what_is_solver.solve).to eq(11) 
   end
+
+  it 'should handle complex what is with plus minus and multipled by' do
+    s = Solver.new("what is 5 plus 2 minus 5 multipled by 2")  
+    expect(s.solve).to eq(4)
+  end
+
   it 'should correctly answer which is' do
     expect(which_is_largest_solver.solve).to eq("123")
   end
